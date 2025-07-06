07f4648be2ad3afde603b5b46f16100db954ba19
alanmoraales <alanmoraales845@gmail.com>
2025-07-05 20:17:00 -0600

    add title to editor

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -5,6 +5,7 @@ import { Placeholder } from "@tiptap/extensions";
 import StarterKit from "@tiptap/starter-kit";
 import { db, Note } from "@/app/database";
 import { useEffect } from "react";
+import { Input } from "@/components/ui/input";
 
 const Editor = ({ note }: { note: Note }) => {
   const editor = useEditor({
@@ -20,7 +21,7 @@ const Editor = ({ note }: { note: Note }) => {
     editorProps: {
       attributes: {
         class:
-          "h-full p-4 prose dark:prose-invert min-w-[65ch] @max-[65ch]:min-w-[100cqw] focus-visible:outline-none",
+          "h-full prose dark:prose-invert min-w-[65ch] @max-[65ch]:min-w-[100cqw] focus-visible:outline-none",
       },
     },
     onBlur: ({ editor }) => {
@@ -32,13 +33,37 @@ const Editor = ({ note }: { note: Note }) => {
   });
 
   useEffect(() => {
-    if (editor && note) {
+    if (editor && note.content !== editor.getHTML()) {
       editor.commands.setContent(note.content);
       editor.commands.focus();
     }
   }, [note, editor]);
 
-  return <EditorContent editor={editor} />;
+  useEffect(() => {
+    console.log("running update note title effect");
+    requestAnimationFrame(() => {
+      const editNoteInput = document.getElementById(
+        `edit-note-title-${note.id}`
+      );
+      if (editNoteInput) {
+        (editNoteInput as HTMLInputElement).value = note.title;
+      }
+    });
+  }, [note]);
+
+  return (
+    <div className="h-full grid gap-4 grid-rows-[auto_1fr] p-4">
+      <Input
+        id={`edit-note-title-${note.id}`}
+        defaultValue={note.title}
+        className="rounded-none px-0 text-3xl! font-bold! focus-visible:outline-none! border-0 shadow-none focus-visible:ring-0 focus-visible:shadow-none"
+        onBlur={(e) => {
+          db.notes.update(note.id, { title: e.target.value });
+        }}
+      />
+      <EditorContent editor={editor} />
+    </div>
+  );
 };
 
 export default Editor;
